169b72210d2b4b2d7977ec0a3baa2104f7204ad4
Suhan Moon <lunyx@me.com>
2018-12-22 16:08:30 +0900

    Initlize

--- example/App.js
+++ example/App.js
@@ -7,34 +7,54 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View, TouchableOpacity} from 'react-native';
+import {Platform, StyleSheet, Text, View, TouchableOpacity, ActivityIndicator} from 'react-native';
 
 import KakaoSDK from 'react-native-ccs-kakaosdk';
 
 type Props = {};
 export default class App extends Component<Props> {
 
-	async login() {
+	state = {
+
+	};
+
+	constructor(props) {
+		super(props);
+	}
+
+	login() {
+		this.setState({ loading: true });
+
 		KakaoSDK.login().then((response) => {
 			console.log(response);
+			this.setState({ loading: false });
 		}).catch(e => {
 			console.log(e);
+			this.setState({ loading: false });
 		});
 	}
 
 	async logout() {
+		this.setState({ loading: true });
+
 		KakaoSDK.logout().then((response) => {
 			console.log(response);
+			this.setState({ loading: false });
 		}).catch(e => {
 			console.log(e);
+			this.setState({ loading: false });
 		});
 	}
 
 	async getToken() {
+		this.setState({ loading: true });
+
 		KakaoSDK.getAccessToken().then(token => {
 			alert(JSON.stringify(token));
+			this.setState({ loading: false });
 		}).catch(e => {
 			console.log(e);
+			this.setState({ loading: false });
 
 		});
 	}
@@ -43,21 +63,21 @@ export default class App extends Component<Props> {
 		return (
 			<View style={styles.container}>
 
-				<TouchableOpacity 
-					onPress={() => this.login() }
-					style={{ borderColor: '#000', borderWidth: 1, padding: 10, width: 200, }}>
+				{(this.state.loading && true) && (
+				<View style={ styles.loading }>
+					<ActivityIndicator />
+				</View>
+				)}
+
+				<TouchableOpacity onPress={() => this.login() } style={ styles.button }>
 					<Text>Login</Text>
 				</TouchableOpacity>
 
-				<TouchableOpacity 
-					onPress={() => this.logout() }
-					style={{ borderColor: '#000', borderWidth: 1, padding: 10, width: 200, marginTop: 10, }}>
+				<TouchableOpacity onPress={() => this.logout() } style={ styles.button }>
 					<Text>Logout</Text>
 				</TouchableOpacity>
 
-				<TouchableOpacity 
-					onPress={() => this.getToken() }
-					style={{ borderColor: '#000', borderWidth: 1, padding: 10, width: 200, marginTop: 10, }}>
+				<TouchableOpacity onPress={() => this.getToken() } style={ styles.button }>
 					<Text>getToken</Text>
 				</TouchableOpacity>
 			</View>
@@ -72,4 +92,20 @@ const styles = StyleSheet.create({
 		alignItems: 'center',
 		backgroundColor: '#F5FCFF',
 	},
+
+	loading : {
+		...StyleSheet.absoluteFill, 
+		zIndex: 10, 
+		backgroundColor: 'rgba(0,0,0,0.8)', 
+		alignItems: 'center', 
+		justifyContent: 'center'
+	},
+
+	button: {
+		borderColor: '#000', 
+		borderWidth: 1, 
+		padding: 10, 
+		width: 200, 
+		margin: 10,
+	}
 });
