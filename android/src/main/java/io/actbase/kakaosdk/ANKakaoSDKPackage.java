283c8afd14bfbebc20dd54547c354c0d5db0a555
Suhan Moon <lunyx@me.com>
2019-11-03 10:38:58 +0900

    android fix

--- android/src/main/java/io/actbase/kakaosdk/ANKakaoSDKPackage.java
+++ android/src/main/java/io/actbase/kakaosdk/ANKakaoSDKPackage.java
@@ -14,17 +14,20 @@ public class ANKakaoSDKPackage implements ReactPackage {
 
     @Override
     public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
-      return Arrays.<NativeModule>asList(new ANKakaoSDKModule(reactContext));
+        return Arrays.<NativeModule>asList(
+                new ANKakaoLogin(reactContext),
+                new ANKakaoLink(reactContext)
+        );
     }
 
     // Deprecated from RN 0.47
     public List<Class<? extends JavaScriptModule>> createJSModules() {
-      return Collections.emptyList();
+        return Collections.emptyList();
     }
 
     @Override
     public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
-      return Collections.emptyList();
+        return Collections.emptyList();
     }
 
 }
