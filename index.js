16b6a7e04f8861765ec6d1807529a5832f468ecb
zusinShinpei <kmh011225@naver.com>
2020-01-17 23:32:08 +0900

    Add: react-native-kakao-navi'

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import { NativeModules } from "react-native";
 
-const { ARNKakaoLogin, ARNKakaoLink, ARNKakaoChannel } = NativeModules;
+const { ARNKakaoLogin, ARNKakaoLink, ARNKakaoChannel, ARNKakaoNavi } = NativeModules;
 
 export const link = {
   sendFeed: object => {
@@ -44,8 +44,34 @@ export const channel = {
   }
 };
 
+export const navi = {
+  share: (location, options = {}, viaList = []) => {
+    if (!location) {
+      console.error(
+        "Cannot call the ANKakaoNavi.share with " + location + " location"
+      );
+    } else if (viaList.length > 3) {
+      console.error("viaList must be <=3");
+    } else {
+      ARNKakaoNavi.share(location, options, viaList);
+    }
+  },
+  navigate: (location, options = {}, viaList = []) => {
+    if (!location) {
+      console.error(
+        "Cannot call the ANKakaoNavi.navigate with " + location + " location"
+      );
+    } else if (viaList.length > 3) {
+      console.error("viaList must be <=3");
+    } else {
+      ARNKakaoNavi.navigate(location, options, viaList);
+    }
+  }
+};
+
 export default {
   link,
   login,
-  channel
+  channel,
+  navi
 };
