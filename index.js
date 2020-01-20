9be0f68e7518529056e8f308dafb79113c3cf1c6
zusinShinpei <kmh011225@naver.com>
2020-01-20 19:17:29 +0900

    Fix: index.js

--- index.js
+++ index.js
@@ -1,22 +1,40 @@
-import { NativeModules } from 'react-native';
+import { NativeModules } from "react-native";
 
 module.exports = {
   get Login() {
     if (!NativeModules.ARNKakaoLogin) {
-      console.warn("Install to Kakao Login - $ npm i @actbase/react-native-kakao-login");
+      console.warn(
+        "Install to Kakao Login - $ npm i @actbase/react-native-kakao-login"
+      );
       return null;
     }
     return NativeModules.ARNKakaoLogin;
-  }
-
+  },
   get Link() {
     if (!NativeModules.ARNKakaoLink) {
-      console.warn("Install to Kakao Link - $ npm i @actbase/react-native-kakao-link");
+      console.warn(
+        "Install to Kakao Link - $ npm i @actbase/react-native-kakao-link"
+      );
       return null;
     }
     return NativeModules.ARNKakaoLink;
+  },
+  get Channel() {
+    if (!NativeModules.ARNKakaoChannel) {
+      console.warn(
+        "Install to Kakao Channel - $ npm i @actbase/react-native-kakao-channel"
+      );
+      return null;
+    }
+    return NativeModules.ARNKakaoChannel;
+  },
+  get Navi() {
+    if (!NativeModules.ARNKakaoNavi) {
+      console.warn(
+        "Install to Kakao Navi - $ npm i @actbase/react-native-kakao-navi"
+      );
+      return null;
+    }
+    return NativeModules.ARNKakaoNavi;
   }
-
 };
-
-
